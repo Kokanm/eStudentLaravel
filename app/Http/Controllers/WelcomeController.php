6ae72af45a73d3564c65cc2790d5f655e98fe050
rootc01 <rootc01@hotmail.com>
2015-03-28 21:44:45 +0100

    simple test

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -30,7 +30,8 @@ class WelcomeController extends Controller {
 	 */
 	public function index()
 	{
-		return view('welcome');
+        return "Hello TPO group19 to laravel";
+		//return view('welcome');
 	}
 
 }
